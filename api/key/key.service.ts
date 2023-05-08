9f745d2b71e20e61983d44d1d6c8e323ac512c75
Zielin0 <me@zielinus.xyz>
2023-05-08 17:46:11 +0200

    fix(api/key): mapping is no more needed for client.fetch()

--- api/key/key.service.ts
+++ api/key/key.service.ts
@@ -1,9 +1,6 @@
 import { Client } from '../../client';
-import { FieldMap } from '../../utils';
 import { Key } from './key.types';
 
-const mapping: FieldMap<Key> = {};
-
 export const getKey = async (client: Client) => {
-  return await client.fetch<Key>('key', mapping);
+  return await client.fetch<Key>('key');
 };
