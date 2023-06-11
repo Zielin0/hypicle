80a19b0611d038e10f6cff420cfc59a57281ef7c
Zielin0 <me@zielinus.xyz>
2023-06-11 14:30:55 +0200

    fix(client): move some functions to utils

--- client.ts
+++ client.ts
@@ -1,4 +1,9 @@
-import { FieldMap } from './utils';
+import {
+  convertKeysToLowerCase,
+  FieldMap,
+  mapObjectFields,
+  searchParamsFromObj,
+} from './utils';
 
 interface Options {
   query?: QueryParams,
@@ -32,61 +37,6 @@ export class HypicleError extends Error {
     this.name = 'HypicleError' + status;
   }
 }
-
-// TODO: Move this to utils
-const searchParamsFromObj = <T extends Record<string, QueryParam>>(obj: T): URLSearchParams => {
-  const params = new URLSearchParams();
-
-  Object.keys(obj).forEach(key => {
-    const value = obj[key];
-
-    if (typeof value === 'undefined') return;
-    if (Array.isArray(value)) {
-      params.set(key, value.join(','));
-    }
-
-    params.set(key, value.toString());
-  });
-
-  return params;
-};
-
-// TODO: Move this to utils
-const convertKeysToLowerCase = (data: any) => {
-  const result: any = {};
-  for (const key in data) {
-    if (Object.prototype.hasOwnProperty.call(data, key)) {
-      const value = data[key];
-      const lowerCaseKey = key.toLowerCase();
-
-      if (typeof value === 'object' && !Array.isArray(value)) {
-        result[lowerCaseKey] = convertKeysToLowerCase(value);
-      } else {
-        result[lowerCaseKey] = value;
-      }
-    }
-  }
-  return result;
-};
-
-// TODO: Move this to utils
-const mapObjectFields = <R extends unknown>(data: any, mapping: FieldMap<R>): R => {
-  const mapped: Partial<R> = {};
-
-  for (const key in data) {
-    if (mapping[key]) {
-      if (typeof mapping[key] === 'string') {
-        mapped[mapping[key] as keyof R] = data[key];
-      } else {
-        mapped[key as keyof R] = mapObjectFields(data[key], mapping[key] as FieldMap<R[keyof R]>);
-      }
-    } else {
-      mapped[key as keyof R] = data[key];
-    }
-  }
-
-  return mapped as R;
-};
 
 export class Hypicle implements Client {
 
--- utils.ts
+++ utils.ts
@@ -1,3 +1,57 @@
+import { QueryParam } from './client';
+
 export interface FieldMap<T> {
   [key: string]: keyof T | FieldMap<T[keyof T]>
 }
+
+export const searchParamsFromObj = <T extends Record<string, QueryParam>>(obj: T): URLSearchParams => {
+  const params = new URLSearchParams();
+
+  Object.keys(obj).forEach(key => {
+    const value = obj[key];
+
+    if (typeof value === 'undefined') return;
+    if (Array.isArray(value)) {
+      params.set(key, value.join(','));
+    }
+
+    params.set(key, value.toString());
+  });
+
+  return params;
+};
+
+export const convertKeysToLowerCase = (data: any) => {
+  const result: any = {};
+  for (const key in data) {
+    if (Object.prototype.hasOwnProperty.call(data, key)) {
+      const value = data[key];
+      const lowerCaseKey = key.toLowerCase();
+
+      if (typeof value === 'object' && !Array.isArray(value)) {
+        result[lowerCaseKey] = convertKeysToLowerCase(value);
+      } else {
+        result[lowerCaseKey] = value;
+      }
+    }
+  }
+  return result;
+};
+
+export const mapObjectFields = <R extends unknown>(data: any, mapping: FieldMap<R>): R => {
+  const mapped: Partial<R> = {};
+
+  for (const key in data) {
+    if (mapping[key]) {
+      if (typeof mapping[key] === 'string') {
+        mapped[mapping[key] as keyof R] = data[key];
+      } else {
+        mapped[key as keyof R] = mapObjectFields(data[key], mapping[key] as FieldMap<R[keyof R]>);
+      }
+    } else {
+      mapped[key as keyof R] = data[key];
+    }
+  }
+
+  return mapped as R;
+};
