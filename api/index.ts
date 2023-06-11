13b0217a6c66f2544e580e7c7b3139200ea1892e
Zielin0 <me@zielinus.xyz>
2023-06-11 13:48:38 +0200

    fix(files): renamed *.service.ts to *.handler.ts

--- api/index.ts
+++ api/index.ts
@@ -1,18 +1,18 @@
 export * from './counts/counts.types';
-export * from './counts/counts.service';
+export * from './counts/counts.handler';
 
 export * from './key/key.types';
-export * from './key/key.service';
+export * from './key/key.handler';
 
 export * from './punishments/punishments.types';
-export * from './punishments/punishments.service';
+export * from './punishments/punishments.handler';
 
 export * from './boosters/boosters.types';
-export * from './boosters/boosters.service';
+export * from './boosters/boosters.handler';
 
 export * from './status/status.types';
-export * from './status/status.service';
+export * from './status/status.handler';
 
 export * from './recentgames/recentgames.types';
-export * from './recentgames/recentgames.service';
+export * from './recentgames/recentgames.handler';
 
