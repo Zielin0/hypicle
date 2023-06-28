dd2b16867cd5e64eb375ca99c3c11b3e91e38082
Zielin0 <me@zielinus.xyz>
2023-06-28 15:36:20 +0200

    fix(api): use property shorthand in fetch

--- api/status/status.handler.ts
+++ api/status/status.handler.ts
@@ -2,5 +2,5 @@ import { Client } from '../../client';
 import { Status } from './status.types';
 
 export const getStatus = async (client: Client, uuid: string) => {
-  return await client.fetch<Status>('status', { query: { 'uuid': uuid } });
+  return await client.fetch<Status>('status', { query: { uuid } });
 };
