99cf90defbf182bd2b8fa6956f24f6af04274a0e
Zielin0 <me@zielinus.xyz>
2023-06-29 19:10:27 +0200

    fix(api): reduce amount of getters on Stats

---
+++ api/player/stats.handler.ts
@@ -0,0 +1,75 @@
+import {
+  ArcadeStats,
+  ArenaStats,
+  BattlegroundStats,
+  BedwarsStats,
+  BuildBattleStats,
+  DuelsStats,
+  GingerBreadStats,
+  HousingStats,
+  HungerGamesStats,
+  LegacyStats,
+  MainLobbyStats,
+  MCGOStats,
+  MurderMysteryStats,
+  PaintballStats,
+  PlayerStats,
+  QuakeStats,
+  SkyBlockStats,
+  SkyWarsStats,
+  SpeedUHCStats,
+  SuperSmashStats,
+  TNTGamesStats,
+  UHCStats,
+  VampireZStats,
+  Walls3Stats,
+  WallsStats,
+  WoolGamesStats,
+} from './player.types';
+import { Player } from './player.handler';
+
+type MinigameStatsMap = {
+  Bedwars: BedwarsStats;
+  SkyWars: SkyWarsStats;
+  Duels: DuelsStats;
+  HungerGames: HungerGamesStats;
+  SuperSmash: SuperSmashStats;
+  Battleground: BattlegroundStats;
+  Arcade: ArcadeStats;
+  MCGO: MCGOStats;
+  GingerBread: GingerBreadStats;
+  VampireZ: VampireZStats;
+  Paintball: PaintballStats;
+  Quake: QuakeStats;
+  Legacy: LegacyStats;
+  TNTGames: TNTGamesStats;
+  UHC: UHCStats;
+  SpeedUHC: SpeedUHCStats;
+  Walls3: Walls3Stats;
+  MurderMystery: MurderMysteryStats;
+  BuildBattle: BuildBattleStats;
+  SkyBlock: SkyBlockStats;
+  Walls: WallsStats;
+  Arena: ArenaStats;
+  WoolGames: WoolGamesStats;
+  MainLobby: MainLobbyStats;
+  Housing: HousingStats;
+};
+
+export class Stats {
+  readonly #player: Player;
+
+  constructor(player: Player) {
+    this.#player = player;
+  }
+
+  async get(): Promise<PlayerStats | undefined> {
+    return (await this.#player.getPlayerData()).player.stats;
+  }
+
+  async getByName<T extends keyof MinigameStatsMap>(
+    minigame: T,
+  ): Promise<MinigameStatsMap[T] | undefined> {
+    return (await this.get())?.[minigame];
+  }
+}
