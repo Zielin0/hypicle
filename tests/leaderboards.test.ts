958fa9f636d3787ee15a5794ef370b987f1ae383
Zielin0 <me@zielinus.xyz>
2023-07-17 23:18:47 +0200

    fix(tests): leaderboards test

--- tests/leaderboards.test.ts
+++ tests/leaderboards.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, test } from '@jest/globals';
+import { getLeaderboards } from '../api';
 import { Hypicle } from '../client';
 import { API_KEY } from './_env';
-import { getLeaderboards } from '../api';
 
 describe('Endpoints', () => {
   describe('/leaderboards', () => {
@@ -11,7 +11,7 @@ describe('Endpoints', () => {
       const leaderboards = await getLeaderboards(client);
 
       expect(leaderboards.success).toBe(true);
-      expect(leaderboards.leaderboards.duels).toBeDefined();
+      expect(leaderboards.leaderboards).toBeDefined();
     });
   });
 });
